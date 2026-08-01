7831a8f359551c41175ca9f3122edc85cfdce41e
aliila1359-art <aliila1359@gmail.com>
2026-08-01 07:42:12 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,73 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <windows.h>
-#include <conio.h> // برای توابع _kbhit و _getch
-
-// تابع برای تغییر وضعیت کلیدهای اختصاصی کیبورد
-void SetKeyboardLED(int led_choice) {
-    BYTE keyState[256];
-    GetKeyboardState(keyState);
-
-    int vk_code = 0;
-    if (led_choice == 1) vk_code = VK_CAPITAL;
-    else if (led_choice == 2) vk_code = VK_NUMLOCK;
-    else if (led_choice == 3) vk_code = VK_SCROLL;
-    else {
-        printf("Invalid LED number (1-3).\n");
-        return;
-    }
-
-    keybd_event(vk_code, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
-    keybd_event(vk_code, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
-    
-    printf("LED %d toggled.\n", led_choice);
-}
+#include <setupapi.h>
+#include <hidsdi.h>
 
 int main() {
-    int choice;
-
-    printf("=== DOS Shell Multi-LED Controller ===\n");
-    printf("Select an LED (1-3) or press [ESC] to exit:\n");
-    printf("  [1] Caps Lock\n");
-    printf("  [2] Num Lock\n");
-    printf("  [3] Scroll Lock\n\n");
-
-    while (1) {
-        printf("C:\\> ");
-        
-        // بررسی هوشمند ورودی عدد یا کلید اسکیپ
-        // در اینجا از scanf استفاده می‌کنیم، اما می‌توانید کلید Esc را هم تست کنید
-        if (scanf("%d", &choice) != 1) {
-            // اگر کاربر کاراکتری غیر از عدد وارد کرد، پاکسازی می‌کنیم
-            while(getchar() != '\n');
-            continue;
-        }
-
-        if (choice >= 1 && choice <= 3) {
-            SetKeyboardLED(choice);
-        }
-        else {
-            printf("Bad command or file name\n");
-        }
-        
-        printf("\nPress ESC to exit, or enter another number:\n");
-        
-        // حلقه داخلی برای انتظار جهت دریافت عدد بعدی یا کلید ESC
-        while (1) {
-            if (_kbhit()) {
-                int ch = _getch();
-                if (ch == 27) { // کد اسکی کلید ESC برابر با 27 است
-                    printf("\nExiting...\n");
-                    return 0;
-                }
-                else if (ch >= '1' && ch <= '3') {
-                    choice = ch - '0';
-                    printf("%d\n", choice);
-                    SetKeyboardLED(choice);
-                    break; // برگشت به حلقه اصلی برای ادامه کار
-                }
-            }
-        }
+    unsigned int input_val;
+    printf("Enter a number to send to Blue Pill: ");
+    if (scanf("%u", &input_val) != 1) {
+        printf("Invalid input.\n");
+        return 1;
     }
 
+    unsigned char target_value = (unsigned char)input_val;
+
+    GUID hidGuid;
+    HidD_GetHidGuid(&hidGuid);
+
+    HDEVINFO deviceInfoTable = SetupDiGetClassDevs(&hidGuid, NULL, NULL, DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
+    if (deviceInfoTable == INVALID_HANDLE_VALUE) {
+        printf("Error: Could not get device list.\n");
+        return 1;
+    }
+
+    SP_DEVICE_INTERFACE_DATA deviceInterfaceData;
+    deviceInterfaceData.cbSize = sizeof(SP_DEVICE_INTERFACE_DATA);
+
+    DWORD memberIndex = 0;
+    HANDLE hDevice = INVALID_HANDLE_VALUE;
+
+    // مقادیر VID و PID دستگاه بلوپیل خود را اینجا وارد کنید
+    USHORT targetVID = 0x0483;
+    USHORT targetPID = 0x5710;
+
+    while (SetupDiEnumDeviceInterfaces(deviceInfoTable, NULL, &hidGuid, memberIndex, &deviceInterfaceData)) {
+        memberIndex++;
+
+        DWORD requiredSize = 0;
+        SetupDiGetDeviceInterfaceDetail(deviceInfoTable, &deviceInterfaceData, NULL, 0, &requiredSize, NULL);
+
+        PSP_DEVICE_INTERFACE_DETAIL_DATA deviceInterfaceDetailData = (PSP_DEVICE_INTERFACE_DETAIL_DATA)malloc(requiredSize);
+        deviceInterfaceDetailData->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA);
+
+        if (SetupDiGetDeviceInterfaceDetail(deviceInfoTable, &deviceInterfaceData, deviceInterfaceDetailData, requiredSize, NULL, NULL)) {
+            HANDLE hTest = CreateFile(deviceInterfaceDetailData->DevicePath, GENERIC_READ | GENERIC_WRITE,
+                                      FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+
+            if (hTest != INVALID_HANDLE_VALUE) {
+                HIDD_ATTRIBUTES attrib;
+                attrib.Size = sizeof(HIDD_ATTRIBUTES);
+                if (HidD_GetAttributes(hTest, &attrib)) {
+                    if (attrib.VendorID == targetVID && attrib.ProductID == targetPID) {
+                        hDevice = hTest;
+                        free(deviceInterfaceDetailData);
+                        break;
+                    }
+                }
+                CloseHandle(hTest);
+            }
+        }
+        free(deviceInterfaceDetailData);
+    }
+
+    SetupDiDestroyDeviceInfoList(deviceInfoTable);
+
+    if (hDevice == INVALID_HANDLE_VALUE) {
+        printf("Error: Blue Pill HID device not found.\n");
+        return 1;
+    }
+
+    // ساخت پکت ارسال (بایت اول Report ID و بایت دوم عدد کاربر)
+    unsigned char buffer[65] = {0}; 
+    buffer[0] = 0;             
+    buffer[1] = target_value;  
+
+    if (HidD_SetOutputReport(hDevice, buffer, sizeof(buffer))) {
+        printf("Successfully sent number %u to Blue Pill.\n", target_value);
+    } else {
+        printf("Error: Failed to send data.\n");
+    }
+
+    CloseHandle(hDevice);
     return 0;
-}
